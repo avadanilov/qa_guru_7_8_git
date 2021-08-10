44a9bc696c36e09c594427dc422cb00bb8061861
Aleksandr Danilov <avadanilov@gmail.com>
2021-08-10 14:57:34 +0300

    fix message

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,9 +1,9 @@
-import org.junit.jupiter.api.Test;
 import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
 
 public class FirstTest {
     @Test
-    void firstTest(){
+    void firstTest() {
         System.out.println("FirstTest !!!");
         Selenide.open("http://ya.ru");
     }
