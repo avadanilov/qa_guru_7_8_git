5099479e78f8c2769a1686727312c0579453a0d1
Aleksandr Danilov <avadanilov@gmail.com>
2021-08-10 14:40:30 +0300

    add logic in first test

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Selenide;
 
 public class FirstTest {
     @Test
-    void firstTest(){}
+    void firstTest(){
+        System.out.println("firstTest");
+        Selenide.open("http://ya.ru");
+    }
 }
