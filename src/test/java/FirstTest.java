e92e45172a6ef2e148a54bc7d365e08123d5da11
Aleksandr Danilov <avadanilov@gmail.com>
2021-08-11 13:38:16 +0300

    fix message 1

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 public class FirstTest {
     @Test
     void firstTest() {
-        System.out.println("FirstTest!");
+        System.out.println("FirstTest1");
         Selenide.open("http://ya.ru");
     }
 }
